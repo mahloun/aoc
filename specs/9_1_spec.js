e6a2a60e37b595b873e67786824fc9c1f53fd43b
mahloun <thms.no@gmail.com>
2016-11-30 17:52:04 +0100

    day 9 1_2 update

--- specs/9_1_spec.js
+++ specs/9_1_spec.js
@@ -2,6 +2,11 @@ const test = require('ava');
 
 const _ = require('../src/9_1');
 
+const road_map = [
+  {from: "Paris", to: "Toronto", weight: 42},
+  {from: "Paris", to: "Berlin", weight: 26},
+]
+
 test('#to_object', t => {
   const output = _.to_object("Paris to Toronto = 42");
 
@@ -10,10 +15,9 @@ test('#to_object', t => {
   t.deepEqual(output.to, "Toronto");
   t.deepEqual(output.weight, 42);
 });
- 
+
 test('#parse', t => {
-  const input = "Paris to Toronto = 42\nParis to Berlin = 32\n";
-  const output = _.parse(input);
+  const output = _.parse("Paris to Toronto = 42\nParis to Berlin = 32\n");
 
   t.deepEqual(output.length, 2, "The size of the returned array should be of length 2");
   t.deepEqual(output[0].from, "Paris");
@@ -22,4 +26,28 @@ test('#parse', t => {
   t.deepEqual(output[1].from, "Paris");
   t.deepEqual(output[1].to, "Berlin");
   t.deepEqual(output[1].weight, 32);
+});
+
+test('#get_cities_reducer', t => {
+  const output = _.get_cities_reducer(new Set, road_map[0]);
+
+  t.true(output.has("Paris"));
+  t.true(output.has("Toronto"));
+});
+
+test('#get_cities', t => {
+  t.deepEqual(_.get_cities(road_map).length, 3, "the returned list of cities does not have duplicate values");
+});
+
+test('#road', t => {
+  const road = _.find_road(road_map);
+  const correct_order = road("Paris", "Berlin");
+  const invert_order = road("Berlin", "Paris");
+
+  t.deepEqual(correct_order, road_map[1]);
+  t.deepEqual(invert_order, road_map[1]);
+});
+
+test('#itinerary_distance', t => {
+  t.deepEqual(_.itinerary_distance(road_map)(['Toronto', 'Paris', 'Berlin']), 68);
 });
--- src/9_1.js
+++ src/9_1.js
@@ -1,26 +1,54 @@
 'use strict';
 
 const my = require('../lib/my');
+const Combinatorics = require('js-combinatorics');
 
-function to_object(road) {
+const to_object = (road) => {
   const [, from, to, weight] = road.match(/^(\w+) to (\w+) = (\d+)$/);
 
   return {from, to, weight: parseInt(weight)};
-}
+};
 
-function parse(input) {
-  return input.trim().split('\n').map(to_object);
-}
+const parse = (input) => input.trim().split('\n').map(to_object);
 
-function routine(input) {
-  my.tap(parse(input))(input =>
-    console.log(input)
-  );
-}
+const get_cities_reducer = (cities, {from, to}) => {
+  cities.add(from);
+  cities.add(to);
+  return cities;
+};
+
+const get_cities = (road_map) => [...road_map.reduce(get_cities_reducer, new Set)];
+
+const find_road = road_map =>
+  (from, to) =>
+    road_map.find(road => (road.from === from && road.to === to) || (road.from === to && road.to === from));
+
+const itinerary_distance = road_map => {
+  const road = find_road(road_map);
+
+  return itinerary =>
+    itinerary.reduce((distance, current_city, i) =>
+        i < itinerary.length - 1 ? distance + road(current_city, itinerary[i + 1]).weight : distance
+    , 0)
+};
+
+const routine = input =>
+  my.tap(parse(input))(road_map => {
+    const path = Combinatorics.permutation(get_cities(road_map))
+      .map(itinerary_distance(road_map))
+      .reduce((preferred_distance, current_distance) =>
+        preferred_distance > current_distance ? preferred_distance : current_distance);
+
+    console.log(path);
+  })
 
 module.exports = {
   name: '9_1',
   to_object,
   parse,
+  get_cities_reducer,
+  get_cities,
+  find_road,
+  itinerary_distance,
   routine
-}
+};
